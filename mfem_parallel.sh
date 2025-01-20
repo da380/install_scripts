8e42d8bbc42fbba9aac8aca4430136da43521dab
david <da380@cam.ac.uk>
2025-01-20 20:16:18 +0000

    mfem examples improved

--- mfem_parallel.sh
+++ mfem_parallel.sh
@@ -5,8 +5,18 @@ PETSC_DIR=$DEV/petsc-install
 
 
 cmake -S $DEV/mfem -B mfem_parallel_build \
-      -DCMAKE_BUILD_TYPE=RELEASE               \
-      -DMFEM_USE_MPI=ON                        \
-      -DMPI_CXX_COMPILER=$PETSC_DIR/mpicxx
+      -DCMAKE_BUILD_TYPE=RELEASE          \
+      -DMFEM_USE_MPI=ON                   \
+      -DHYPRE_DIR=$PETSC_DIR              \
+      -DMETIS_DIR=$PETSC_DIR          
+
+
+cmake --build mfem_parallel_build -j 20
+
+cmake --build mfem_parallel_build -t examples -j 20
+
+cmake --build mfem_parallel_build -t tests -j 20
+
+cmake --build mfem_parallel_build -t miniapps -j 20
 
 
--- mfem_serial.sh
+++ mfem_serial.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-cmake -S $HOME/dev/mfem -B mfem_serial_build -DCMAKE_BUILD_TYPE=RELEASE
-cmake --build mfem_serial_build -j 20
+cmake -S $HOME/dev/mfem -B mfem_serial_build \
+      -DCMAKE_CXX_COMPILER=g++-13            \
+      -DCMAKE_BUILD_TYPE=RELEASE
 
-cmake -S $HOME/dev/glvis -B glvis_build -DMFEM_DIR=mfem_serial_build -DCMAKE_BUILD_TYPE=RELEASE
+cmake --build mfem_serial_build -j 20
+cmake --build mfem_serial_build -t tests -j 20
+cmake --build mfem_serial_build -t examples -j 20
+cmake --build mfem_serial_build -t miniapps -j 20
+
+
+
+cmake -S $HOME/dev/glvis -B glvis_build  \
+      -DCMAKE_CXX_COMPILER=g++-13        \
+      -DMFEM_DIR=mfem_serial_build       \
+      -DCMAKE_BUILD_TYPE=RELEASE
+
 cmake --build glvis_build -j 20
