a6183d91d4f54973e511d6e270dd33dbf6b22c43
David Al-Attar <da380@cam.ac.uk>
2026-04-21 09:17:19 +0100

    added script for installing gmsh

--- raja_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cmake -S $HOME/dev/raja -B raja_cuda_build \
-      -DCMAKE_CXX_COMPILER=g++-13          \
-      -DCMAKE_BUILD_TYPE=RELEASE           \
-      -DENABLE_CUDA=ON                     \
-      -DENABLE_OPENMP=ON                   \
-      -DCMAKE_CUDA_ARCHITECTURES=native
